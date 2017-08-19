64f3619c2e94194f25bc745eafee023de4d561e9
Elvin Thudugala <elvin.thudugala@outlook.com>
2017-08-20 01:11:47 +1200

    Remove unwanted files

--- TMTControls/packages/PhoneNumberFormatter.1.0.4/tools/install.ps1
+++
@@ -1,6 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
-
-$countryCodes = $project.ProjectItems.Item("CountryCodes.txt")
-# set 'Copy To Output Directory' to 'Copy if newer'
-$copyToOutput = $countryCodes.Properties.Item("CopyToOutputDirectory")
-$copyToOutput.Value = 2
